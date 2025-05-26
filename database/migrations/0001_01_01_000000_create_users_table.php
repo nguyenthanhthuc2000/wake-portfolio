d1498e24fc593af2eadec6dd5484f160fd1e0e6b
n_thanhthuc <n_thanhthuc@thk-hd.vn>
2025-05-26 11:41:39 +0700

    master | migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('introduce')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('git_url')->nullable();
+            $table->string('facebook_url')->nullable();
+            $table->string('linkedin_url')->nullable();
+            $table->string('cv_url')->nullable();
+            $table->string('image_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
