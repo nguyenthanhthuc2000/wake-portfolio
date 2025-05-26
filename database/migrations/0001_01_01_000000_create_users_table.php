3cd71b145de7bcca8974271e20afbd7b76b924e6
n_thanhthuc <n_thanhthuc@thk-hd.vn>
2025-05-26 11:50:40 +0700

    update migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('linkedin_url')->nullable();
             $table->string('cv_url')->nullable();
             $table->string('image_url')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
